a3b29d2a7c6f28dac53297a48ccc0f7bf242b270
Dell Schulz <dellschulz@gmail.com>
2018-01-06 20:13:31 -0200

    Update and rename aicp.mk to du.mk

--- du.mk
+++ du.mk
@@ -17,9 +17,9 @@
 $(call inherit-product, device/xiaomi/mido/full_mido.mk)
 
 # Inherit some common AICP stuff.
-$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
 
-PRODUCT_NAME := aicp_mido
+PRODUCT_NAME := du_mido
 BOARD_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
